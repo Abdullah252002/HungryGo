4053ad8dd7ee44e5c68ad3376c5b437e144647c2
abdullah <abdoihesham1966@gmail.com>
2024-04-28 16:30:09 +0300

    save user

--- app/src/main/java/com/example/hungrygo/splash.kt
+++ app/src/main/java/com/example/hungrygo/splash.kt
@@ -5,18 +5,79 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import com.example.hungrygo.app.home.customer.Customer_home
+import com.example.hungrygo.app.home.delivery.Delivery_home
+import com.example.hungrygo.app.home.restaurant.Restaurant_home
 import com.example.hungrygo.app.login.Login
+import com.example.hungrygo.app.model.appUser_customer
+import com.example.hungrygo.app.model.appUser_delivery
+import com.example.hungrygo.app.model.appUser_restaurant
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 
 class splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash)
-        Handler(Looper.getMainLooper()).postDelayed({gotologin()},3000)
+        Handler(Looper.getMainLooper()).postDelayed({ Navigate() }, 3000)
 
     }
 
-    private fun gotologin() {
-        val intent=Intent(this,Login::class.java)
+    private fun Navigate() {
+        val firebaseUser = Firebase.auth.currentUser
+        if (firebaseUser == null) {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+            finish()
+        } else {
+            login_customer_tofirestore(firebaseUser.uid,
+                OnSuccessListener {
+                    val user = it.toObject(appUser_customer::class.java)
+                    if(user!=null){
+                    DataUtils.appUser_customer = user
+                    start_customer_home()}
+                })
+            login_delivery_tofirestore(firebaseUser.uid,
+                OnSuccessListener {
+                    val user = it.toObject(appUser_delivery::class.java)
+                    if (user!=null){
+                    DataUtils.appuser_Delivery = user
+                    start_delivery_home()}
+                },
+                OnSuccessListener {
+                    val imageUrl = it.toString()
+                    /*
+                    Glide.with(this)  // Use 'this' if you are inside an Activity, or 'context' if inside a Fragment or other context
+                        .load(imageUrl)
+                        .into(image)
+                     */
+
+                })
+            login_resturant_tofirestore(firebaseUser.uid!!,
+                OnSuccessListener {
+                    val user = it.toObject(appUser_restaurant::class.java)
+                    if(user!=null){
+                    DataUtils.appuser_Restaurant = user
+                    start_restaurant_home()}
+                })
+        }
+    }
+
+    fun start_customer_home() {
+        val intent = Intent(this, Customer_home::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    fun start_delivery_home() {
+        val intent = Intent(this, Delivery_home::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    fun start_restaurant_home() {
+        val intent = Intent(this, Restaurant_home::class.java)
         startActivity(intent)
         finish()
     }
