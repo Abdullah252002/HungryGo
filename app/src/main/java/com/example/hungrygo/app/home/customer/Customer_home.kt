f356814be45b813ab4349b92b05a98ebcbc6925e
abdullah <abdoihesham1966@gmail.com>
2024-04-28 20:21:23 +0300

    set color and navigation

--- app/src/main/java/com/example/hungrygo/app/home/customer/Customer_home.kt
+++ app/src/main/java/com/example/hungrygo/app/home/customer/Customer_home.kt
@@ -1,13 +1,21 @@
 package com.example.hungrygo.app.home.customer
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.hungrygo.R
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 
 class Customer_home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.customer_home)
+
+        val id=findViewById<TextView>(R.id.id)
+        id.setOnClickListener {
+            Firebase.auth.signOut()
+        }
 
     }
 }
--- app/src/main/java/com/example/hungrygo/app/home/restaurant/Restaurant_home.kt
+++ app/src/main/java/com/example/hungrygo/app/home/restaurant/Restaurant_home.kt
@@ -1,13 +1,23 @@
 package com.example.hungrygo.app.home.restaurant
 
 import android.os.Bundle
+import android.view.Menu
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.hungrygo.R
+import com.example.hungrygo.databinding.ActivityRestaurantHomeBinding
 
 class Restaurant_home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.restaurant_home)
-
+        setContentView(R.layout.activity_restaurant_home)
     }
+
 }
